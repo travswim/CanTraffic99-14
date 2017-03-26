0c6f74962de2af7cd90cb90d8b5ad5f168bf8405
Travis Booth <travswim@gmail.com>
2017-03-25 17:02:34 -0700

    Don't have weather stats not working as list Going to change to dictionary

--- Code/cantraffic.py
+++ Code/cantraffic.py
@@ -8,8 +8,12 @@ various statistics of the data available in NCDB_1999_to_2014.csv """
 # Import statements
 import numpy as np
 import pandas as pd
+import random
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import timing
+import math
+from itertools import product
 # import os.path
 import pickle
 
@@ -21,8 +25,9 @@ import pickle
 #     df = pd.read_pickle('cantraffic.pk1')
 # Read in .csv file
 df = pd.read_csv("NCDB_1999_to_2014.csv")
-fat = []
-nonfatal = []
+start = 1999
+end = 2014
+
 def fatal(x, year):
     """ fatal(int, int)
 
@@ -40,7 +45,6 @@ def sex(x, sex, year):
                 'M', male """
 
     return((df['C_YEAR'] == year)&(df['P_SEX'] == sex)).loc[(df['C_SEV'] == x)].sum()
-
 
 def weather(x, w, year):
     """ weather(int, string, int)
@@ -120,30 +124,77 @@ def day(x, d, year):
 
     return((df['C_YEAR'] == year)&(df['C_WDAY'] == h)).loc[(df['C_SEV'] == x)].sum()
 
-# def plotcharts(start_year, end_year, stat):
-#
-#     N = end_year - start_year;
-#
-#     menMeans = (20, 35, 30, 35, 27)
-#     womenMeans = (25, 32, 34, 20, 25)
-#     # menStd = (2, 3, 4, 1, 2)
-#     # womenStd = (3, 5, 2, 3, 3)
-#     ind = np.arange(N)    # the x locations for the groups
-#     width = 0.35       # the width of the bars: can also be len(x) sequence
-#
-#     p1 = plt.bar(ind, menMeans, width, color='#d62728', yerr=menStd)
-#     p2 = plt.bar(ind, womenMeans, width, bottom=menMeans, yerr=womenStd)
-#
-#     plt.ylabel('Scores')
-#     plt.title('Scores by group and gender')
-#     plt.legend((p1[0], p2[0]), ('Men', 'Women'))
-#     plt.xticks(ind, tuple(map(str, range(start_year, end_year + 1))))
-#     plt.yticks(np.arange(0, 5000, 200))
-#
-#
-#     plt.show()
+def pop_random(lst):
+    idx = random.randrange(0, len(lst) +1 )
+    return lst.pop(idx)
 
-for year in range(start_year, end_year + 1):
-    fat.append(fatal(1, year))
-    nonfat.append(fatal(2, year))
-print fat
+def plotfatal():
+    fat = []
+    nonfat = []
+
+    for year in range(start, end + 1):
+        fat.append(fatal(1, year))
+        nonfat.append(fatal(2, year))
+
+    percentage = map(float,np.array(fat))/(np.array(fat)+np.array(nonfat))*100
+    print [round(elem, 2) for elem in percentage]
+
+    N = end - start + 1;
+    ind = np.arange(N)    # the x locations for the groups
+    width = 0.50       # the width of the bars: can also be len(x) sequence
+    p1 = plt.bar(ind, tuple(fat), width, color='r')
+    plt.ylabel('Number of accidents')
+    plt.title('Canadian Fatal Vehicle Accidents by Year')
+    plt.xticks(ind, tuple(map(str, range(start, end + 1))))
+    plt.yticks(np.arange(0, 8000, 1000))
+
+    plt.show()
+
+def rounder(num):
+    a = list(map(int,str(num)))
+    for i in range(1, len(a)):
+        x = math.pow(10, i)
+    return int((a[0]+1)*x)
+
+
+def plotweather(feature, variable, start, end):
+    colours = ['r', 'b', 'k', 'w', 'm', 'g', 'c', 'y', 'yellow', 'aqua']
+    legend = {'1': 'sunny', '2': 'cloudy', '3': 'rainny', '4': 'snowing', \
+    '5': 'sleet', '6': 'visibility', '7': 'windy', 'Q': 'other', 'U': 'unknown',\
+     'X': 'N/A'}
+    N = end - start + 1;
+    pairs = {}
+    l = len(legend)
+    # feature = 'C=_WTHR'
+    ind = np.arange(N)    # the x locations for the groups
+    width = 0.50
+    # max_feature = [0]*(N)
+    plot = []
+    plot2 = []
+    for key, value in legend.items():
+        pairs["{0}".format(key)] = colours.pop()
+
+    for item in df.feature.unique():
+        "list{0}".format(item) = []
+        "p{0}".format(item) = []
+        for year in range(start, end + 1):
+            "list{0}".format(item).append(weather(1, item, year))
+            "p{0}".format(item).append(ind, tuple("list{0}".format(item)), width, color = pairs[item])
+            plot.append("p{0}[0]".format(item))
+            plot2.append(legend[item])
+        # max_feature = [x + y for x, y in zip(max_feature, "list{0}".format(item)]
+        print "list{0}".format(item)
+    #         "p{0}".format(x) = plt.bar(ind, tuple)
+    # percentage = map(float,np.array(fat))/(np.array(fat)+np.array(nonfat))*100
+    # print [round(elem, 2) for elem in percentage]
+
+
+           # the width of the bars: can also be len(x) sequence
+    # p1 = plt.bar(ind, tuple(fat), width, color='r')
+    plt.ylabel('Number of accidents')
+    plt.title("Canadian Fatal Vehicle Accidents by Year due to {0}".format(variable))
+    plt.xticks(ind, tuple(map(str, range(start, end + 1))))
+    plt.yticks(np.arange(0, rounder(max(max_feature)), rounder(max(max_feature))/10))
+    plt.legend(tuple(plot), tuple(plot2))
+    # plt.show()
+plotweather('C_WTHR', 'Weather', 1999, 2014)
