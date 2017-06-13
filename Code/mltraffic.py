240181e594394b5de650d128ea865fb5dd66a13c
Travis Booth <travswim@gmail.com>
2017-06-13 10:31:02 -0700

    Removed nan, removed duplicates, and wrongly formatted input data. Preprocessing and One hot encoding will be next

--- Code/mltraffic.py
+++ Code/mltraffic.py
@@ -22,14 +22,89 @@ print "Total number of fatalities: {}".format(fatal)
 print "Total number of non-fatalities: {}".format(nonFatal)
 
 print "Total number of Accidents: {}".format(total_accidents)
-del fatal, nonFatal, total_accidents
+del fatal, nonFatal
 #Find the feature columns
 feature_cols = list(df.drop('C_SEV', 1))
 #Find the target columns
-target_col = df['C_SEV']
+target_col = 'C_SEV'
 #Print some more stuff
 # print "Feature columns:\n{}".format(feature_cols)
 # print "\nTarget column: {}".format(target_col)
+
+
+
+
+# Show the feature information by printing the first five rows
+# print "\nFeature values:"
+# print X_all.head()
+
+# Data Formatiing: Lets see if the data is uniform
+
+# This function is to replace wrong input data with the correct ones
+# (it would have been easier if the data had been entered correctly :/)
+
+# Print list of items for a given feature
+def data(feature):
+    print list(df[feature].unique())
+
+
+# Find most common item
+def most_common(lst):
+    return max(set(lst), key=lst.count)
+
+# Replace Null values with most common element
+def is_null(feature):
+    df[feature] = df[feature].replace('nan', most_common(list(df[feature])))
+    df[feature] = df[feature].replace('', most_common(list(df[feature])))
+
+# Find the data type of an item
+def data_type(feature):
+    # for feature in feature_cols:
+    tp = []
+    for item in list(df[feature].unique()):
+        tp.append(type(item))
+    print feature
+    print tp
+
+# Check and replace items not formated as strings
+def not_string(feature):
+    for item in df[feature].unique():
+        if type(item) is not str:
+            df[feature] = df[feature].replace(item, str(item))
+
+
+# Check if all items in a list are the same length
+def length(feature):
+    ln = []
+    for item in list(df[feature].unique()):
+        ln.append(len(str(item)))
+    # Check if all items in list are the same
+    return all(x == ln[0] for x in ln)
+# print "C_VEHS"
+# data("C_VEHS")
+
+## As we can see, not only is the data of mixed types (int and string, supposed
+## to be stings) its also contains duplicates ('1' and '01 are equivalent') and 'nan'.
+
+variable_count = 0
+
+# This section calls various functions above and adds '0' to '1'
+for feature in feature_cols:
+    # print feature
+    # data(feature)
+    not_string(feature)
+    is_null(feature)
+    if length(feature) == False:
+        for item in df[feature].unique():
+
+            if len(item) == 1:
+                df[feature] = df[feature].replace(item, "0" + item)
+
+# Count the number of unique items to deal with preprocessing, PCA and the curse of dimensionality
+    variable_count += len(list(df[feature].unique()))
+print "Featire Space: {0}".format(variable_count)
+print "Data length: {0}".format(total_accidents)
+# We have enough data points to work with
 
 # Separate the data into feature data and target data (X_all and y_all, respectively)
 try:
@@ -39,40 +114,5 @@ try:
     # print "Features: {}".format(feature_cols)
 
     print "Successfully separated features and target"
-    del target_col
 except:
     print "Failed to separate features and target"
-
-
-# Show the feature information by printing the first five rows
-# print "\nFeature values:"
-# print X_all.head()
-
-# This function is to replace wrong input data with the correct ones
-# (it would have been easier if the data had been entered correctly :/)
-def replace(item, newitem):
-    df[feature].replace(item, newitem)
-# Preprocessing: Lets see if the data is uniform
-# unique_items = {}
-# for feature in feature_cols:
-#     for item in list(df[feature].unique()):
-#         if len(item <=2):
-#
-#     print feature
-#     print list(df[feature].unique())
-# print list(df['C_SEV'].unique())
-# unique = pd.DataFrame(unique_items)
-# print unique
-for feature in feature_cols:
-    # print feature
-    lst = list(df[feature].unique())
-    for item in lst:
-        if re.match('[1-9]', item) != None:
-            repla = "0" + item
-            ## need to use regex to match numbers, otherwise might not work for UU, U or XX, X
-            ## also need to deal with NaN or N/A or blank spaces
-            print item print repla
-            lst.remove(item)
-                df[feature].replace(item, repla)
-# df[year].unique():
-# def data_fix()
